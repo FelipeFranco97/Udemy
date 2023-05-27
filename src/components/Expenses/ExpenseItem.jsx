dd3d9c6ed687efaa3e5fb43c45dc8c2025955094
FelipeFranco97 <48889338+FelipeFranco97@users.noreply.github.com>
2023-05-26 23:21:31 -0500

    48. Working with "State"

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,17 +1,21 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) =>{
+
+    const [title,setTitle] = useState(props.title);   
+
     const clickHandler = () =>{
-        console.log('Clicked!!!!')
+        setTitle('Updated!');
     };
 
     return (
         <div className='expense-item'>
            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
