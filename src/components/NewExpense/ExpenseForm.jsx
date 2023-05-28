1b2761987e98ad6351acc87e975f2efc98523e2b
FelipeFranco97 <48889338+FelipeFranco97@users.noreply.github.com>
2023-05-28 14:58:22 -0500

    54. Using One State Instead (And What's Better)

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,45 +1,65 @@
-import { useState } from 'react';
-import './ExpenseForm.css';
+import { useState } from "react";
+import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
+  const [inputUser, setInputUser] = useState({
+    enteredTitle:'',
+    enteredAmount:'',
+    enteredDate:'',
+  })
 
-    const titleChangeHandler = () => {
-        setEnteredTitle();
-    };
+  const titleChangeHandler = () => {
+    setInputUser({
+        ...inputUser,
+        enteredTitle: '',
+    });
+  };
 
-    const amountChangeHandler = () =>{
-        setEnteredAmount();
-    };
+  const amountChangeHandler = () => {
+    setInputUser({
+        ...inputUser,
+        enteredAmount: '',
+    });
+  };
 
-    const dateChangeHandler = () =>{
-        setEnteredDate();
-    };
+  const dateChangeHandler = () => {
+    setInputUser({
+        ...inputUser,
+        enteredDate: '',
+    });
+  };
 
-    return(
-        <form action="">
-            <div className='new-expense__controls'>
-                <div className='new-expense__control'>
-                    <label htmlFor="">Title</label>
-                    <input type="text" onChange={titleChangeHandler}/>
-                </div>
-                <div className='new-expense__control'>
-                    <label htmlFor="">Amount</label>
-                    <input type="number" min='0.01' step='0.01'onChange={amountChangeHandler}/>
-                </div>
-                <div className='new-expense__control'>
-                    <label htmlFor="">Title</label>
-                    <input type="date" min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler}/>
-                </div>
-            </div>
-            <div className='new-expense__actions'>
-                <button type='submit'>Add Expense</button>
-            </div>
-        </form>
-    )
-}
+  return (
+    <form action="">
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label htmlFor="">Title</label>
+          <input type="text" onChange={titleChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label htmlFor="">Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label htmlFor="">Title</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
 
-export default ExpenseForm
+export default ExpenseForm;
