b6dcbcc52b9a87287c3bc3456b9c6e8bf99f1d64
FelipeFranco97 <48889338+FelipeFranco97@users.noreply.github.com>
2023-05-30 18:42:51 -0500

    60. Controlled vs Uncontrolled Components & Stateless vs Stateful Components

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,26 +1,18 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react';
-import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
-const ExpenseItem = (props) =>{
-
-    const [title,setTitle] = useState(props.title);   
-
-    const clickHandler = () =>{
-        setTitle('Updated!');
-    };
-
-    return (
-        <div className='expense-item'>
-           <ExpenseDate date={props.date}/>
-            <div className='expense-item__description'>
-                <h2>{title}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
-            </div>
-            <button onClick={clickHandler}>Change Title</button>
-        </div>
-    )
+const ExpenseItem = (props) => {
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </Card>
+  );
 }
 
 export default ExpenseItem;
